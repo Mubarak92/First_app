4b9548c4eb5a41dd45ac730b69a1fca3aa78d906
Mubarak92 <Mubarak.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-10-12 14:49:04 +0300

    first project

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+fun main() {
+    println("Hello World!")
+    val name = "Mubarak"
+    val number = 7
+    val flag = true
+    val pi =3.14
+
+    println(name)
+    println(number)
+    println(flag)
+    println(pi)
+
+    println("my name is ${name} and my favoraite number ${number}")
+
+    if(number > 5){
+
+        println("number bigger than 5")
+
+    }
+    else {
+
+        println("number less than 5")
+    }
+
+   if(number== 7){
+
+       println("number is equale to 7 ")
+
+   }
+    // Try adding program arguments at Run/Debug configuration
+    //println("Program arguments: ${args.joinToString()}")
+}
